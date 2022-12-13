839adc831365488370ee0196e76934ddd5b013dc
Dalibor Mrška <mrda20ma@student.ju.se>
2022-12-13 22:37:41 +0100

    Playing around with config and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/tailwind.css'],
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      noscript: [{ children: 'Javascript is required' }],
+    },
+  },
 })
